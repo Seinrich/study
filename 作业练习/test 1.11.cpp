5091a5665c8fd2a886f076e972d202d97f1932a6
Seinrich <sylx_gsts@163.com>
2022-01-13 22:58:50 +0800

    homework

--- 作业练习/test 1.11.cpp
+++ 作业练习/test 1.11.cpp
@@ -2,35 +2,104 @@
 
 #include<stdio.h>
 
-int Max(int x, int y)
-{
-	return x > y ? x : y;
-}
+//
 
+//int Max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//
+////int main()
+////{
+////	int a = 0;
+////	int b = 0;
+////	scanf("%d %d", &a, &b);
+////	printf("最大值是 %d\n", Max(a, b));
+////
+////}
+//
 //int main()
 //{
 //	int a = 0;
 //	int b = 0;
+//	int i = 0;
+//	int j = 0;
 //	scanf("%d %d", &a, &b);
-//	printf("最大值是 %d\n", Max(a, b));
+//	for (i = 0; i <= a; i++)
+//	{
+//		for (j = 0; j <= a; j++)
+//		{
+//			if (a == i + j && b == 2 * i + 4 * j)
+//			{
+//				printf("%d %d", i, j);
+//			}
+//		}
+//	}
+//}
+
+//void Print(int n)
+//{
+//    if (n)
+//    {
+//        printf("%d", n % 10);
+//        n /= 10;
+//        Print(n);
+//    }
+//    else
+//    {
+//        return;
+//    }
+//}
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    Print(n);
+//} 
+
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b))
+//	{
+//		printf("1");
+//	}
+//}
+
+#include "stdio.h"
+
+//int main()
+//{
+//    /*int array[100002] = { 0 };
+//    int n;
+//    scanf("%d", &n);
+//    int temp;
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &temp);
+//        if (array[temp])
+//        {
+//            printf("YES");
+//            return 0;
+//        }
+//        else
+//            array[temp] = 1;
+//    }
 //
+//    printf("NO");
+//    return 0;*/
 //}
 
 int main()
 {
 	int a = 0;
-	int b = 0;
-	int i = 0;
-	int j = 0;
-	scanf("%d %d", &a, &b);
-	for (i = 0; i <= a; i++)
+	int b = 1;
+	int c = 2;
+	if (c > a > b)
 	{
-		for (j = 0; j <= a; j++)
-		{
-			if (a == i + j && b == 2 * i + 4 * j)
-			{
-				printf("%d %d", i, j);
-			}
-		}
+		printf("1");
 	}
 }
+
+
