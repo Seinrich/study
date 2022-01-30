d4a1f5020e253044a75a361d734509cf3fbdebea
Seinrich <sylx_gsts@163.com>
2022-01-30 21:46:17 +0800

    归并排序、快排

---
+++ 12.30创建的/排序算法.c
@@ -0,0 +1,160 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void swap(int* p, int x, int y)
+{
+	int tmp = p[x];
+	p[x] = p[y];
+	p[y] = tmp;
+}
+
+//选择排序
+
+void insertsort(int* p, int sz)
+{
+	int i,j;
+	for (i = 1; i < sz; i++) // 0到i处要有序
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (p[j] > p[j + 1])
+			{
+				int tmp = p[j];
+				p[j] = p[j + 1];
+				p[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+void test01()
+{
+	int arr[] = { 1,2,3,41,2,21,3,46,345,12 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	insertsort(arr, sz);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+//归并排序
+
+void merge(int* p, int left, int mid, int right);
+
+void mergesort(int* p, int left,int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	int mid = left + ((right - left) >> 1);
+	mergesort(p, left, mid);
+	mergesort(p, mid + 1, right);
+	merge(p,left,mid,right);
+}
+
+void merge(int* p, int left, int mid, int right)
+{
+	int p1 = left;
+	int p2 = mid + 1;
+	int i = 0;
+
+	int* phelp = (int*)malloc(sizeof(int) * (right - left + 1));
+	while (p1 <= mid && p2 <= right)
+	{
+		phelp[i++] = p[p1] < p[p2] ? p[p1++] : p[p2++];
+	}
+	while (p1 <= mid)
+	{
+		phelp[i++] = p[p1++];
+	}
+	while (p2 <= right)
+	{
+		phelp[i++] = p[p2++];
+	}
+	for (i = 0; i <= right - left; i++)
+	{
+		p[left + i] = phelp[i];
+	}
+	free(phelp);
+}
+
+void test02()
+{
+	int arr[] = { 41,2,21,3,46,345,12 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	mergesort(arr, 0, sz - 1);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+//快速排序，left是待排序数组第一个元素下标，right是最后一个元素下标
+
+int* partition(int* arr,int left, int right)
+{
+	int lessarea = left - 1; //小于中轴数的右边界
+	int morearea = right; //大于中轴数的左边界(不+1是因为之前交换了中轴数位置)
+
+	while (left<morearea)
+	{
+		if (arr[left] > arr[right])
+		{
+			swap(arr, left, --morearea);
+		}
+		else if (arr[left] < arr[right])
+		{
+			swap(arr, left++, ++lessarea);
+		}
+		else
+		{
+			left++;
+		}
+	}
+	swap(arr, right, morearea++); //将中轴数交换回去
+	int* pboundary = (int*)malloc(sizeof(int) * 2);
+	pboundary[0] = lessarea;
+	pboundary[1] = morearea;
+
+	return pboundary;
+}
+
+void quicksort(int* arr, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	int pivot = left + (rand() % (right - left + 1)); //确定出随机中轴数下标，保证在left与right之间
+	swap(arr, pivot, right);//将其换到数组最后的位置
+	int* pboundary = partition(arr,left,right); //存放排序后的大小边界位置
+	quicksort(arr, left, pboundary[0]);
+	quicksort(arr, pboundary[1], right);
+
+}
+
+
+void test03()
+{
+	int arr[] = { 2,1,34534,324,134,6344,235454,1,12,3,4 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	quicksort(arr,0,sz-1);
+	int i = 0;
+	for (; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	//test01();
+	//test02();
+	test03();
+}
